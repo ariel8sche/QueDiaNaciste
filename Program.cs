4aa1ed97ae9bbdd1cf3cd14a723d91fbeac0ff68
ariel8sche <ariel8sche@gmail.com>
2025-03-03 19:34:09 -0300

    Subiendo archivos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace QueDiaNaciste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, año;
+            string nacimiento;
+            Console.Write("\nIngrese el Dia de nacimiento: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.Write("\nIngrese el Mes de nacimiento: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.Write("\nIngrese el Año de nacimiento: ");
+            año = int.Parse(Console.ReadLine());
+            DateTime fecha = new DateTime(año, mes, dia);
+            //Console.WriteLine(fecha.ToString("dddd", new CultureInfo("es-ES")));
+            nacimiento = fecha.ToString("dddd", new CultureInfo("es-ES"));
+            Console.WriteLine("\nEl dia de la semana en que naciste es: " + nacimiento);
+        }
+    }
+}
+
